312e426baaa8eaeffb74b114c79cc2994ba17172
wb <wblucky@126.com>
2017-01-30 16:59:31 +0800

    关系运算符

---
+++ test2.lua
@@ -0,0 +1,5 @@
+print(1 < 2)  --output:true
+print(1 == 2)  --output:false
+print(1 ~= 2)  --output:true
+local a, b = true, false
+print(a == b)  --output:false
---
+++ test3.lua
@@ -0,0 +1,9 @@
+local a = { x = 1, y = 0}
+local b = { x = 1, y = 0}
+if a == b then
+  print("a==b")
+else
+  print("a~=b")
+end
+
+--output:a~=b
